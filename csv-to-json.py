4df6cb39c9ca2a7c4bbc95964fee168076782be4
Daniel Ye <danielxinxin1@gmail.com>
2020-11-25 21:09:17 -0300

    modificando script de dados e adicionando dados corrigidos

--- csv-to-json.py
+++ csv-to-json.py
@@ -9,5 +9,7 @@ df['trump_votes'] = np.where(df['trailing_candidate_name'] == 'Trump', df['trail
 df['biden_votes'] = np.where(df['trailing_candidate_name'] == 'Trump', df['leading_candidate_votes'], df['trailing_candidate_votes'])
 df['trump_percentage'] = df['trump_votes'] / (df['trump_votes'] + df['biden_votes'])
 
+df['state'] = df['state'].str.replace(r"(\s*\(.*?\)\s*)", " ").str.strip()
+
 df.drop(columns = ['trailing_candidate_name', 'leading_candidate_name', 'trailing_candidate_votes', 'leading_candidate_votes'], inplace=True)
 df.to_json("election-filtered.json", orient="records")
