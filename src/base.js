6249038228d9c1ab14d214f590fbf7a3ab173db3
moutella <joaogabrielmsc@gmail.com>
2020-11-28 21:43:56 -0300

    beta 1

--- src/base.js
+++ src/base.js
@@ -2,6 +2,7 @@ import {
   ElectionView
 } from './electionView.js'
 
+let currentIndex = 0
 $(document).ready(() => {
   var elems = document.querySelectorAll('.collapsible');
   var instances = M.Collapsible.init(elems, {
@@ -10,22 +11,40 @@ $(document).ready(() => {
 });
 
 
-async function readJson(filepath){
+async function readJson(filepath) {
   let json = await d3.json(filepath)
   return json
 }
-async function main(){
-  let params = {
-    selector: '#main',
-    width: 960,
-    height: 500
-  }
-  let electionView = new ElectionView(params)
+
+let params = {
+  selector: '#main',
+  width: 960,
+  height: 500
+}
+let electionView = new ElectionView(params)
+let possibleTimes = null;
+let voting_data = null
+async function main() {
   let geographic_limits = await readJson('us-states.json')
   electionView.updateGeography(geographic_limits);
-  let voting_data = await readJson('election-filtered.json')
+  voting_data = await readJson('election-filtered.json')
+
+  let timestampSet = new Set()
+  for (let data of voting_data) {
+    timestampSet.add(data['timestamp'].split(":")[0])
+  }
+  possibleTimes = Array.from(timestampSet)
+  possibleTimes = possibleTimes.sort();
+  console.log(possibleTimes);
   electionView.updateData(voting_data);
-  electionView.render();
 }
 
 main()
+jQuery("#slider-range").on("input", function () {
+  let currentLimit = possibleTimes[$(this).val()]
+  console.log(currentLimit)
+  let currentData = voting_data.filter(item =>
+    item.timestamp <= currentLimit)
+  console.log(currentData.length);
+  electionView.updateData(currentData)
+})
--- src/electionView.js
+++ src/electionView.js
@@ -6,7 +6,7 @@ export class ElectionView {
     this.svg = null;
     this.tooltips = null;
 
-    
+
     let projection = d3.geoAlbersUsa().scale(800);
 
     this.color = d3.scaleSequential(d3.interpolateRdBu).domain([0.6, 0.4]);
@@ -26,9 +26,9 @@ export class ElectionView {
       .attr("class", "tooltip")
       .style("opacity", 0);
 
-      var offsets = $(this.params.selector).offset();
-      this.top = offsets.top;
-      this.left = offsets.left;
+    var offsets = $(this.params.selector).offset();
+    this.top = offsets.top;
+    this.left = offsets.left;
   }
 
   createMargins() {
@@ -41,26 +41,29 @@ export class ElectionView {
     this.geography = geography
   }
   updateData(voting_data) {
-    console.log(self.geography)
-    this.geography.features.forEach(function (state, i) {
+    console.log(this.geography)
+    this.final_geo = this.geography
+    this.final_geo.features.forEach(function (state, i) {
       let state_data = voting_data.find(data =>
         data.state == state.properties.name)
       if (state_data) {
         let trumpWins = state_data.trump_percentage > 0.5;
-        state.properties.color = d3.scaleSequential(d3.interpolateRdBu).domain([0.6, 0.4])(state_data.trump_percentage)
+        //state.properties.color = d3.scaleThreshold().domain([0.55, 0.45]).range(['blue', 'white', 'red'])(state_data.trump_percentage)
+       
         state.properties.winner = trumpWins ? "Trump" : "Biden";
-        if (trumpWins){
+        if (trumpWins) {
           state.properties.percentual = (state_data.trump_percentage * 100).toFixed(2);
-        }
-        else{
-          state.properties.percentual = ((1-state_data.trump_percentage) * 100).toFixed(2);
+          state.properties.color = 'red'
+        } else {
+          state.properties.percentual = ((1 - state_data.trump_percentage) * 100).toFixed(2);
+          state.properties.color = 'blue'
         }
         state.properties.evs = state_data.electoral_votes
         state.properties.updateTimestamp = state_data.timestamp
       }
 
     });
-
+    this.render()
   }
 
   render() {
@@ -69,22 +72,27 @@ export class ElectionView {
     var left = this.left
     var top = this.top
     this.svg.selectAll("path")
-      .data(this.geography.features)
-      .enter()
-      .append("path")
+      .data(this.final_geo.features)
+      .join("path")
+      .transition()
+      .duration(this.params.duration || 500)
       .attr("d", this.path)
       .style("stroke", "#fff")
       .style("stroke-width", "1")
       .style("fill", function (d) {
+        console.log(d.properties.color)
         return d.properties.color
-      }).on("mouseover", function (event, data) {
+      })
+    this.svg.selectAll("path")
+      .data(this.final_geo.features)
+      .join("path").on("mouseover", function (event, data) {
         div.transition()
           .duration(200)
           .style("opacity", .9);
         div.text(`${data.properties.name}: ${data.properties.percentual}%\n` +
-         `Na frente: ${data.properties.winner}\n` +
-         `Votos Eleitorais: ${data.properties.evs}\n` +
-         `Data: ${data.properties.updateTimestamp.split('.')[0]}`)
+            `Na frente: ${data.properties.winner}\n` +
+            `Votos Eleitorais: ${data.properties.evs}\n` +
+            `Data: ${data.properties.updateTimestamp.split('.')[0]}`)
           .style("left", (d3.pointer(event)[0] + left) + "px")
           .style("top", (d3.pointer(event)[1] + top - 75) + "px");
       })
@@ -95,8 +103,6 @@ export class ElectionView {
           .duration(500)
           .style("opacity", 0);
       })
-      .transition()
-      .duration(this.params.duration || 2000);
 
   }
 
